041035eb7037a11bc625a3642833c2069682bbe5
Luan Henrique <luanhsantos14@gmail.com>
2024-08-06 14:16:14 -0300

    refactor: melhorar rotas e função organizar pautas

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,10 @@ import { json } from 'body-parser';
 import { upload } from './middlewares/uploadMiddleware';
 import { extractInfoMiddleware } from './middlewares/extractInfoMiddleware';
 import { processExcel } from './services/excelProcessor';
-import { createAudiencia, getAudiencias, getAudienciaById, updateAudiencia, deleteAudiencia, exportAudiencias, getAudienciasByFilters } from './controllers/audienciaController';
-import { organizePautas } from './controllers/pautaController';
 import { resetData } from './controllers/resetController';
-import { organizeAudienciasInPautas } from './services/PautaService';
 import routes from './routes/routes';
+import { organizeAudienciasInPautas } from './utils/organizePautas';
+import { setupSwagger } from './swagger';
 
 const app = express();
 
@@ -16,6 +15,8 @@ app.use(cors());
 app.use(json());
 
 app.use(routes)
+
+setupSwagger(app);
 
 // Rota para upload de arquivos Excel
 app.post('/upload', upload.single('file'), extractInfoMiddleware, async (req, res) => {
@@ -29,23 +30,7 @@ app.post('/upload', upload.single('file'), extractInfoMiddleware, async (req, re
   }
 });
 
-// Rotas de CRUD para audiências
-app.post('/audiencias', createAudiencia);
-app.get('/audiencias', getAudiencias);
-app.get('/audiencias/:id', getAudienciaById);
-app.put('/audiencias/:id', updateAudiencia);
-app.delete('/audiencias/:id', deleteAudiencia);
-
-// Rota para organizar pautas
-app.post('/organize-pautas', organizePautas);
-
 // Rota para resetar dados
 app.post('/reset-data', resetData);
-
-// Rota para exportar audiências para Excel
-app.get('/export-audiencias', exportAudiencias);
-
-// Rota pde GET com filtros
-app.get('/audiencias-filter', getAudienciasByFilters);
 
 export default app;
